513164d65e79f277f58f12c638db105a83c8302d
leprechaun <leprechaun@gmail.com>
2025-04-09 00:07:46 +0700

    HelloWorld/Main: A simple test

--- artifacts/HelloWorld/Main/src/main/resources/script/dosomething.groovy
+++ artifacts/HelloWorld/Main/src/main/resources/script/dosomething.groovy
@@ -4,20 +4,13 @@ https://help.sap.com/viewer/368c481cd6954bdfa5d0435479fd4eaf/Cloud/en-US/148851b
 If you want to know more about the SCRIPT APIs, refer the link below
 https://help.sap.com/doc/a56f52e1a58e4e2bac7f7adbf45b2e26/Cloud/en-US/index.html */
 import com.sap.gateway.ip.core.customdev.util.Message;
+
 def Message processData(Message message) {
     //Body
-    def body = message.getBody();
-/*To set the body, you can use the following method. Refer SCRIPT APIs document for more detail*/
-    //message.setBody(body + " Body is modified");
-    //Headers
-    def headers = message.getHeaders();
-    def value = headers.get("oldHeader");
-    message.setHeader("oldHeader", value + " modified");
-    message.setHeader("newHeader", "newHeader");
-    //Properties
-    def properties = message.getProperties();
-    value = properties.get("oldProperty");
-    message.setProperty("oldProperty", value + " modified");
-    message.setProperty("newProperty", "newProperty");
+    def body = message.getBody(java.lang.String);
+    def myheader = message.getHeader("MyHeader", java.lang.String)
+    message.setProperty("xProp", "asdasdasd")
+    message.setProperty("xPropTwo", "qweqweqwe");
+    message.setHeader("MyHeader", myheader.toUpperCase())
     return message;
 }
---
+++ artifacts/HelloWorld/Main/src/test/groovy/testDoSomethingSpec.groovy
@@ -0,0 +1,40 @@
+import com.sap.gateway.ip.core.customdev.util.Message
+import spock.lang.Shared
+import spock.lang.Specification
+// import org.apache.commons.io.IOUtils
+
+class DoSomethingSpec extends Specification {
+    @Shared Script script
+    @Shared Message msg
+
+
+
+    static def xml
+
+
+    def setupSpec() {
+        GroovyShell shell = new GroovyShell()
+        script = shell.parse(
+            new File(this.getClass().getResource('/script/dosomething.groovy').getFile())
+        )
+    }
+
+    def setup() {
+        msg = new Message()
+        msg.setHeader("MyHeader", "some value")
+    }
+
+    def 'Should do ... something'() {
+        given:
+        msg.setBody(this.getClass().getResource("message.xml").newInputStream())
+
+        when:
+        'Script is executed'
+        script.processData(msg)
+
+        then:
+        //1 * test.setProperty("xProp", "asdasdasd")
+        msg.getProperty("xProp") == "asdasdasd"
+        msg.getHeader("MyHeader", String) == "SOME VALUE"
+    }
+}
